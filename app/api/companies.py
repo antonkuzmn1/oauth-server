f412e51ab446fa776d4502601adc1d982c824565
anton <antonkuzmn1@gmail.com>
2025-02-16 02:21:10 +0700

    added security logic for companies api

--- app/api/companies.py
+++ app/api/companies.py
@@ -1,41 +1,113 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
 from app.core.db import get_db
-from app.schemas.company import CompanyOut, CompanyBase, CompanyCreate
+from app.dependecies.auth import get_current_owner, get_current_admin, get_current_user
+from app.schemas.admin import AdminOut
+from app.schemas.company import CompanyOut, CompanyBase, CompanyCreate, CompanyUpdate
+from app.schemas.owner import OwnerOut
+from app.schemas.user import UserOut
 from app.services.company_service import CompanyService
 
 router = APIRouter(prefix="/companies", tags=["companies"])
 
 
 @router.get("/", response_model=List[CompanyOut])
-def get_all_companies(db: Session = Depends(get_db)):
+def get_all_companies(
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+        current_admin: Optional[AdminOut] = Depends(get_current_admin),
+        current_user: Optional[UserOut] = Depends(get_current_user),
+):
     service = CompanyService(db)
-    return service.get_all()
+
+    if current_owner:
+        return service.get_all()
+
+    if current_admin:
+        return service.get_all_by_admin(current_admin)
+
+    if current_user:
+        return service.get_all_by_user(current_user)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.get("/{company_id}", response_model=CompanyOut)
-def get_company_by_id(company_id: int, db: Session = Depends(get_db)):
+def get_company_by_id(
+        company_id: int,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+        current_admin: Optional[AdminOut] = Depends(get_current_admin),
+        current_user: Optional[UserOut] = Depends(get_current_user),
+):
     service = CompanyService(db)
-    return service.get_by_id(company_id)
+
+    if current_owner:
+        return service.get_by_id(company_id)
+
+    if current_admin:
+        return service.get_by_id_by_admin(company_id, current_admin)
+
+    if current_user:
+        return service.get_by_id_by_user(company_id, current_user)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.post("/", response_model=CompanyOut)
-def create_company(company: CompanyCreate, db: Session = Depends(get_db)):
-    service = CompanyService(db)
-    return service.create(company)
+def create_company(
+        company: CompanyCreate,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+):
+    if current_owner:
+        service = CompanyService(db)
+        return service.create(company)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.put("/{company_id}", response_model=CompanyOut)
-def update_company(company_id: int, company: CompanyBase, db: Session = Depends(get_db)):
-    service = CompanyService(db)
-    return service.update(company_id, company)
+def update_company(
+        company_id: int,
+        company: CompanyUpdate,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+):
+    if current_owner:
+        service = CompanyService(db)
+        return service.update(company_id, company)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.delete("/{company_id}", response_model=CompanyOut)
-def delete_company(company_id: int, db: Session = Depends(get_db)):
-    service = CompanyService(db)
-    return service.delete(company_id)
+def delete_company(
+        company_id: int,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+):
+    if current_owner:
+        service = CompanyService(db)
+        return service.delete(company_id)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
--- app/services/company_service.py
+++ app/services/company_service.py
@@ -1,11 +1,72 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
-from typing import Optional
+from typing import Optional, List
 
+from app.models import Company
 from app.repositories.company_repo import CompanyRepository
+from app.schemas.admin import AdminOut
 from app.schemas.company import CompanyOut, CompanyCreate, CompanyUpdate
+from app.schemas.user import UserOut
 from app.services.base_service import BaseService
 
 
 class CompanyService(BaseService[CompanyRepository]):
     def __init__(self, db: Session):
         super().__init__(CompanyRepository(db), CompanyOut)
+        self.db = db
+
+    def get_all_by_admin(self, current_admin: AdminOut) -> List[CompanyOut]:
+        company_ids = [company.id for company in current_admin.companies]
+        if not company_ids:
+            return []
+
+        stmt = (
+            select(Company)
+            .filter(Company.id.in_(company_ids))
+            .distinct()
+        )
+
+        companies = self.db.scalars(stmt).all()
+        return [CompanyOut.model_validate(company) for company in companies]
+
+    def get_all_by_user(self, current_user: UserOut) -> Optional[CompanyOut]:
+        company_id = current_user.company_id
+        if not company_id:
+            return None
+
+        stmt = (
+            select(Company)
+            .filter(Company.id == company_id)
+            .distinct()
+        )
+        company = self.db.scalars(stmt).first()
+        return CompanyOut.model_validate(company) if company else None
+
+    def get_by_id_by_admin(self, company_id: int, current_admin: AdminOut) -> Optional[CompanyOut]:
+        company_ids = [company.id for company in current_admin.companies]
+        if not company_ids:
+            return None
+
+        stmt = (
+            select(Company)
+            .filter(Company.id.in_(company_ids))
+            .filter(Company.id == company_id)
+            .distinct()
+        )
+
+        company = self.db.scalars(stmt).first()
+        return CompanyOut.model_validate(company) if company else None
+
+    def get_by_id_by_user(self, company_id: int, current_user: UserOut) -> Optional[CompanyOut]:
+        company_id = current_user.company_id
+        if not company_id:
+            return None
+
+        stmt = (
+            select(Company)
+            .filter(Company.id == company_id)
+            .distinct()
+        )
+
+        company = self.db.scalars(stmt).first()
+        return CompanyOut.model_validate(company) if company else None
