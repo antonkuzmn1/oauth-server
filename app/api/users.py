e69c55b9274713fc450a8c41f535d869aed358dd
anton <antonkuzmn1@gmail.com>
2025-02-16 12:49:09 +0700

    user logic done

--- app/api/users.py
+++ app/api/users.py
@@ -1,50 +1,149 @@
-from typing import List, Annotated
+from typing import List, Annotated, Optional
 
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.core.db import get_db
-from app.schemas.user import UserOut, UserBase
+from app.dependecies.auth import get_current_user, get_current_owner, get_current_admin
+from app.schemas.admin import AdminOut
+from app.schemas.owner import OwnerOut
 from app.schemas.token import Token
+from app.schemas.user import UserOut, UserUpdate, UserCreate
 from app.services.user_service import UserService
-
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
+@router.get("/profile", response_model=UserOut)
+async def get_user_profile(
+        current_user: Annotated[UserOut, Depends(get_current_user)],
+):
+    if current_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+        )
+
+    return current_user
+
+
 @router.get("/", response_model=List[UserOut])
-def get_all_users(db: Session = Depends(get_db)):
+def get_all_users(
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+        current_admin: Optional[AdminOut] = Depends(get_current_admin),
+        current_user: Optional[UserOut] = Depends(get_current_user),
+):
     service = UserService(db)
-    return service.get_all()
+
+    if current_owner:
+        return service.get_all()
+
+    if current_admin:
+        return service.get_all_by_admin(current_admin)
+
+    if current_user:
+        return service.get_all_by_user(current_user)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.get("/{user_id}", response_model=UserOut)
-def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+def get_user_by_id(
+        user_id: int,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+        current_admin: Optional[AdminOut] = Depends(get_current_admin),
+        current_user: Optional[UserOut] = Depends(get_current_user),
+):
     service = UserService(db)
-    return service.get_by_id(user_id)
+
+    if current_owner:
+        return service.get_by_id(user_id)
+
+    if current_admin:
+        return service.get_by_id_by_admin(user_id, current_admin)
+
+    if current_user:
+        return service.get_by_id_by_user(user_id, current_user)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.post("/", response_model=UserOut)
-def create_user(user: UserBase, db: Session = Depends(get_db)):
+def create_user(
+        user: UserCreate,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+        current_admin: Optional[AdminOut] = Depends(get_current_admin),
+):
     service = UserService(db)
-    return service.create(user)
+
+    if current_owner:
+        return service.create(user)
+
+    if current_admin:
+        return service.create_by_admin(user, current_admin)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.put("/{user_id}", response_model=UserOut)
-def update_user(user_id: int, user: UserBase, db: Session = Depends(get_db)):
+def update_user(
+        user_id: int,
+        user: UserUpdate,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+        current_admin: Optional[AdminOut] = Depends(get_current_admin),
+):
     service = UserService(db)
-    return service.update(user_id, user)
+
+    if current_owner:
+        return service.update(user_id, user)
+
+    if current_admin:
+        return service.update_by_admin(user_id, user, current_admin)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.delete("/{user_id}", response_model=UserOut)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
+def delete_user(
+        user_id: int,
+        db: Session = Depends(get_db),
+        current_owner: Optional[OwnerOut] = Depends(get_current_owner),
+        current_admin: Optional[AdminOut] = Depends(get_current_admin),
+):
     service = UserService(db)
-    return service.delete(user_id)
+
+    if current_owner:
+        return service.delete(user_id)
+
+    if current_admin:
+        return service.delete_by_admin(user_id, current_admin)
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
 
 
 @router.post("/login", response_model=Token)
-def login_for_user_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+def login_for_user_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                db: Session = Depends(get_db)):
     service = UserService(db)
     user = service.authenticate_user(form_data.username, form_data.password)
     access_token = UserService.create_user_token(user)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,15 +1,105 @@
+from typing import List, Optional
+
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from app.models import User
 from app.repositories.user_repo import UserRepository
-from app.schemas.user import UserOut
-
-from app.services.base_service import BaseService
+from app.schemas.admin import AdminOut
+from app.schemas.user import UserOut, UserCreate, UserUpdate
 from app.services.auth_service import AuthService
+from app.services.base_service import BaseService
 
 
 class UserService(BaseService[UserRepository]):
     def __init__(self, db: Session):
         super().__init__(UserRepository(db), UserOut)
+        self.db = db
+
+    def get_all_by_admin(self, current_admin: AdminOut) -> List[UserOut]:
+        company_ids = [company.id for company in current_admin.companies]
+        if not company_ids:
+            return []
+
+        stmt = (
+            select(User)
+            .filter(User.company_id.in_(company_ids))
+            .distinct()
+        )
+        users = self.db.scalars(stmt).all()
+        return [UserOut.model_validate(user) for user in users]
+
+    def get_all_by_user(self, current_user: UserOut) -> List[UserOut]:
+        company_id = current_user.company_id
+        if not company_id:
+            return []
+
+        stmt = (
+            select(User)
+            .filter(User.company_id == company_id)
+            .distinct()
+        )
+        users = self.db.scalars(stmt).all()
+        return [UserOut.model_validate(user) for user in users]
+
+    def get_by_id_by_admin(self, user_id: int, current_admin: AdminOut) -> Optional[UserOut]:
+        company_ids = [company.id for company in current_admin.companies]
+        if not company_ids:
+            return None
+
+        stmt = (
+            select(User)
+            .filter(User.company_id.in_(company_ids))
+            .filter(User.id == user_id)
+            .distinct()
+        )
+
+        user = self.db.scalars(stmt).first()
+        return UserOut.model_validate(user) if user else None
+
+    def get_by_id_by_user(self, user_id: int, current_user: UserOut) -> Optional[AdminOut]:
+        company_id = current_user.company_id
+        if not company_id:
+            return None
+
+        stmt = (
+            select(User)
+            .filter(User.company_id == company_id)
+            .filter(User.id == user_id)
+            .distinct()
+        )
+
+        user = self.db.scalars(stmt).first()
+        return UserOut.model_validate(user) if user else None
+
+    def create_by_admin(self, user: UserCreate, current_admin: AdminOut) -> Optional[UserOut]:
+        company_ids = [company.id for company in current_admin.companies]
+
+        if not user.company_id or user.company_id not in company_ids:
+            return None
+
+        return self.create(user)
+
+    def update_by_admin(self, user_id: int, user: UserUpdate, current_admin: AdminOut) -> Optional[UserOut]:
+        company_ids = [company.id for company in current_admin.companies]
+        old_company_id = self.get_by_id(user_id).company_id
+
+        if not old_company_id or old_company_id not in company_ids:
+            return None
+
+        if not user.company_id or user.company_id not in company_ids:
+            return None
+
+        return self.update(user_id, user)
+
+    def delete_by_admin(self, user_id: int, current_admin: AdminOut) -> Optional[UserOut]:
+        company_ids = [company.id for company in current_admin.companies]
+        old_company_id = self.get_by_id(user_id).company_id
+
+        if not old_company_id or old_company_id not in company_ids:
+            return None
+
+        return self.delete(user_id)
 
     def authenticate_user(self, username: str, password: str):
         user = self.repository.get_by_username(username)
